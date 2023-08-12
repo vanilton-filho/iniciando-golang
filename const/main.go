275e854e919c1130c1ed10f5db26dd1b3d43f11a
Vanilton Filho <vanilton.alv3sfilho@gmail.com>
2023-08-11 21:20:58 -0300

    feat: learn consts in go

---
+++ const/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+const pi float32 = 3.14
+
+const (
+	A string = "teste"
+	b        = 2
+	c        = 3
+)
+
+func main() {
+
+	const xpto = 10
+
+	fmt.Println(xpto)
+}
